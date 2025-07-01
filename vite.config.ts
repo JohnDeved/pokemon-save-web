82099486deff86d1efb8b955d7c844f722337c31
undefined <johann@objekt.stream>
2025-07-01 09:33:02 +0200

    chore: add @million/lint dependency and integrate with Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+import MillionLint from '@million/lint';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [MillionLint.vite({
+    enabled: true
+  }), react(), tailwindcss()],
 })
