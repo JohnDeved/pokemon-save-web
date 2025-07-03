580aa52d54e9a33db6d398e960d232f94319f94b
undefined <johann@objekt.stream>
2025-07-03 19:14:48 +0200

    fix: update minimum height of PokemonMovePlaceholder component

--- src/components/pokemon/PokemonMovePlaceholder.tsx
+++ src/components/pokemon/PokemonMovePlaceholder.tsx
@@ -3,7 +3,7 @@ import { cn } from '../../lib/utils';
 
 export const PokemonMovePlaceholder: React.FC = () => {
     return (
-        <div className={cn("w-full text-left p-3 rounded-lg bg-transparent border border-dashed border-slate-800 flex flex-col justify-between min-h-[72px]")}> 
+        <div className={cn("w-full text-left p-3 rounded-lg bg-transparent border border-dashed border-slate-800 flex flex-col justify-between min-h-[74px]")}> 
             <div className="flex items-center justify-between">
                 <span className="text-sm text-slate-400 font-semibold">——</span>
             </div>
